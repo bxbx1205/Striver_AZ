0b59932120bd511c57d8c176e69a2a975f7cf5b0
sarvesh <srbhanarkar05@gmail.com>
2025-12-29 15:14:38 +0530

    more qn solved

---
+++ src/Array/Easy/LongestSubarraywithSumK.java
@@ -0,0 +1,46 @@
+package Array.Easy;
+
+import java.util.HashMap;
+
+public class LongestSubarraywithSumK {
+
+    public static int longestSubarrayWithSumK(int[] arr, long k) {
+
+        HashMap<Long, Integer> preSumMap = new HashMap<>();
+
+        long sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            // Case 1: subarray from index 0
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            long rem = sum - k;
+
+            // Case 2: subarray exists
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            // Store prefix sum only if not already present
+            // (important for longest length)
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 1, 1};
+        long k = 3;
+
+        System.out.println(longestSubarrayWithSumK(arr, k)); // Output: 3
+    }
+}
--- src/Array/Easy/missinginArray.java
+++ src/Array/Easy/missinginArray.java
@@ -3,18 +3,20 @@ package Array.Easy;
 public class missinginArray {
     static void main(String[] args) {
         int[] arr = {1, 2, 3, 5};
-        System.out.println(missingNum(arr));
+        System.out.println(missingNumber(arr));
 
     }
-    static int missingNum(int[] arr){
-        int n = arr.length;
-        int expected = ((n+1)*(n))/2;
+    static int missingNumber(int[] nums){
 
-        int sum =0;
-        for (int i = 0; i < arr.length-1 ; i++) {
-            sum+=arr[i];
+        int xor =0;
+        for (int i = 0; i <= nums.length; i++) {
+            xor=xor^i;
         }
-        int answer = expected-sum;
-        return answer;
+
+        for (int i = 0; i <nums.length ; i++) {
+            xor=xor^nums[i];
+        }
+
+        return xor;
     }
 }
